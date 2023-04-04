e38b6f13016a72d9a938c33ebd60cc64e6c43072
jSohnData <jihos@me.com>
2023-04-04 15:55:05 -0400

    ADD dbconnect and tvLibrary

---
+++ src/tvLibrary.js
@@ -0,0 +1,21 @@
+import {db} from "./dbConnect.js";
+
+export function addNewTv(req, res) {
+  const newTv = req.body;
+
+  // CREATE: Add New Tv Show
+  db.collection('tvShows')
+    .add(newTv)
+    .then( doc => res.status(201).send(`New Tv Added: ${doc.id}`) )
+    .catch( err => res.status(500).send(err) )
+}
+
+export async function getAllTv(req,res) {
+  const collection = await db.collection('tvShows')
+    .get()
+    .catch(err => res.status(500).send(err) )
+
+    const tvShowList = collection.docs.map(
+      tvShow => ( {...tvShow.data(), id: tvShow.id} )
+    )
+}

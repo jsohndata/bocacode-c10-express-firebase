8ad0558adf4787f823929a016cd09c45a163715e
jSohnData <jihos@me.com>
2023-04-04 16:03:04 -0400

    UPDATE sample edit for class

--- src/tvLibrary.js
+++ src/tvLibrary.js
@@ -3,12 +3,12 @@ import {db} from "./dbConnect.js";
 export function addNewTv(req, res) {
   const newTv = req.body;
 
-  // CREATE: Add New Tv Show
   db.collection('tvShows')
     .add(newTv)
     .then( doc => res.status(201).send(`New Tv Added: ${doc.id}`) )
     .catch( err => res.status(500).send(err) )
 }
+
 
 export async function getAllTv(req,res) {
   const collection = await db.collection('tvShows')
